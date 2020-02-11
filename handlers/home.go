b33a83b151d13c52e946b5f755277d67702a9cb6
Bryan Sandoval <bmsandoval@gmail.com>
2020-02-10 20:43:13 -0700

    improve build process (#1)
    * updated docker container to compile code inside container
    
    * working state
    
    * updating readme

--- handlers/home.go
+++ handlers/home.go
@@ -6,5 +6,5 @@ import (
 
 // home is a simple HTTP handler function which writes a response.
 func home(w http.ResponseWriter, _ *http.Request) {
-	w.Write([]byte("your mom went to college"))
+	w.Write([]byte("Your mom went to college"))
 }
